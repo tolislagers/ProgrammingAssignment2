2aff210124e7096fca2de04a5cb9ae856a1a1340
tolislagers <tom.olislagers@telenet.be>
2015-03-20 22:53:00 +0100

    Completed the makeCacheMatrix part succesfully

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,11 @@
-## Here will my comments be
-## Just copy pasted the example, adapted function names
+## Testing makeCacheMatrix
+## a <- matrix(1:4, 2, 2) -> this will be our testing square matrix
+## Run the makeCacheMatrix function 
+## cm <- makeCacheMatrix(a) -> to make all functions available via cm$
+## ia <- solve(a) -> to create an inverted matrix of a
+## cm$getsolve() -> see that nothing is stored yet
+## cm$setsolve(ia) -> store the inverted matrix
+## cm$getsolve() -> check if the inverted matrix was well stored
 
 makeCacheMatrix <- function(x = matrix()) {
         s <- NULL
@@ -8,7 +14,7 @@ makeCacheMatrix <- function(x = matrix()) {
                 s <<- NULL
         }
         get <- function() x
-        setsolve <- function(solve) s <<- mean
+        setsolve <- function(solve) s <<- solve
         getsolve <- function() s
         list(set = set, get = get,
              setsolve = setsolve,
